a983c969d7464285d9aaff3a9d9f8c463f19d0d0
gotsira <gotmadagascar@gmail.com>
2019-07-02 13:12:54 +0700

    add blur head image

--- src/themes/sellsuki/components/core/blocks/MainSlider/HeadImage.vue
+++ src/themes/sellsuki/components/core/blocks/MainSlider/HeadImage.vue
@@ -1,34 +1,44 @@
 <template>
   <no-ssr>
-    <carousel
-      :per-page="1"
-      :mouse-drag="true"
-      :autoplay="true"
-      :pagination-enabled="false"
-      :loop="true"
-      :autoplay-timeout="5000"
-      :speed="1500"
-      ref="carousel">
-      <slide v-for="currentImage in allImages"
-             :key="currentImage.title"
-             ref="slide"
-             class="slide">
-        <a class="head-image w-100" href="">
-          <img class="carousel-image" src="../../../../assets/banner.jpg" alt="">
-          <div class="head-image-content">
-            <p class="title" data-testid="mainSliderTitle">
-              {{ currentImage.title }}
-            </p>
-            <p
-              class="subtitle mb0"
-              data-testid="mainSliderSubtitle"
-            >
-              {{ currentImage.subtitle }}
-            </p>
-          </div>
-        </a>
-      </slide>
-    </carousel>
+    <div class="container">
+      <carousel
+        :per-page="1"
+        :mouse-drag="true"
+        :autoplay="true"
+        :pagination-enabled="false"
+        :loop="true"
+        :autoplay-timeout="5000"
+        :speed="1500"
+        ref="carousel">
+        <slide v-for="currentImage in allImages"
+               :key="currentImage.title"
+               ref="slide"
+               class="slide">
+          <a class="head-image w-100" href="">
+            <img class="carousel-image" src="../../../../assets/banner.jpg" alt="">
+            <div class="image-content">
+              <p class="title" data-testid="mainSliderTitle">
+                {{ currentImage.title }}
+              </p>
+              <p
+                class="subtitle mb0"
+                data-testid="mainSliderSubtitle"
+              >
+                {{ currentImage.subtitle }}
+              </p>
+            </div>
+          </a>
+        </slide>
+      </carousel>
+      <div class="blur">
+        <img class="blur-image" src="../../../../assets/banner.jpg" alt="">
+        <div class="image-content">
+          <p class="blur-title">
+            BLUR COLLECTION
+          </p>
+        </div>
+      </div>
+    </div>
   </no-ssr>
 </template>
 
@@ -58,13 +68,37 @@ export default {
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css?family=Nunito+Sans&display=swap');
 
+* {
+  font-family: 'Nunito Sans', sans-serif;
+  text-align: center;
+  color: #EEEEEE;
+}
+
+.container {
+  display: block;
+  padding: 0;
+}
+
+.blur {
+  position: relative;
+  z-index: -1;
+}
+
 .carousel-image {
   width: 100%;
   padding: 0;
   display: block;
 }
 
-.head-image-content {
+.blur-image {
+  width: 100%;
+  padding: 0;
+  filter: blur(8px);
+  transform: scale(1.1);
+  z-index: -1;
+}
+
+.image-content {
   transition: unset !important;
   position: absolute;
   top: 50%;
@@ -72,25 +106,19 @@ export default {
   transform: translate(-50%, -50%);
 }
 
-.title {
-  font-family: 'Nunito Sans', sans-serif;
+.blur-title, .title {
   font-weight: 800;
   font-size: 30px;
-  text-align: center;
   letter-spacing: 0.1em;
   line-height: 41px;
   white-space: nowrap;
-  color: #EEEEEE;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.75);
+  text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.75);
 }
 
 .subtitle{
-  font-family: 'Nunito Sans', sans-serif;
-  text-align: center;
   font-weight: 600;
   font-size: 15px;
   line-height: 20px;
-  color: #EEEEEE;
 }
 
 a {
